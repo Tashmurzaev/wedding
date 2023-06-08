cea81a54ffa170c34ba02ef3082fccedd2a3a92a
Sakydin Tashmurzaev <Sakydin@MacBook-Pro-Mac-2.local>
2023-06-08 16:53:25 +0600

    change texts and colors

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ height: "1000vh" }}>
       <Navbar />
     </div>
   );
