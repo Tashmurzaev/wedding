b516969a59bb3316dffec56a9cde2079f436a27f
Sakydin Tashmurzaev <Sakydin@MacBook-Pro-Mac-2.local>
2023-06-14 14:23:48 +0600

    create a invitation component

--- src/components/invitation/Invitation.jsx
+++ src/components/invitation/Invitation.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import cl from "./Invitation.module.css";
 import Title from "../title/Title";
+import invitation from "../../assets/icons/invitation.svg";
 
 const Invitation = () => {
   return (
     <section className={cl.container}>
       <div className={cl.overlay} />
-      <Title style={{ color: "#fff" }} h2="Приглашение" />
+      <Title style={{ color: "#fff" }} h2="Invitation" />
+      <div className={cl.innerContainer}>
+        <div className={cl.rightSide}>
+          <img src={invitation} alt="invitation" />
+          <p>Save </p>
+          <span> THE</span>
+          <p>Date</p>
+        </div>
+      </div>
     </section>
   );
 };
