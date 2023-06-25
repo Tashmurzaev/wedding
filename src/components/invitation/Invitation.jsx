f938bf498a572d8cd3eec3405b6bcf0eeac82e57
Sakydin Tashmurzaev <Sakydin@MacBook-Pro-Mac-2.local>
2023-06-25 15:34:19 +0600

    done invitation

--- src/components/invitation/Invitation.jsx
+++ src/components/invitation/Invitation.jsx
@@ -10,10 +10,25 @@ const Invitation = () => {
       <Title style={{ color: "#fff" }} h2="Invitation" />
       <div className={cl.innerContainer}>
         <div className={cl.rightSide}>
-          <img src={invitation} alt="invitation" />
+          <img className={cl.img} src={invitation} alt="invitation" />
           <p>Save </p>
           <span> THE</span>
           <p>Date</p>
+          <img className={cl.img2} src={invitation} alt="invitation" />
+        </div>
+        <div className={cl.leftSide}>
+          <h2>
+            Kurapika <small>&amp;</small> Alluka
+          </h2>
+          <p>Request the honor of your presence on their wedding day</p>
+
+          <div className={cl.date}>
+            Jule 24, 2023<small>at 06:00 pm</small>
+          </div>
+          <div className={cl.uppercase}>
+            Kaynar, банкетный <br /> зал, Сухэ-Батора, 1/2, Бишкек
+          </div>
+          <h5>Reception to follow</h5>
         </div>
       </div>
     </section>
