3f53f2fd880841e6a364b4c32d05e44a25e92c81
Sakydin Tashmurzaev <Sakydin@MacBook-Pro-Mac-2.local>
2023-07-09 14:47:33 +0600

    motion

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import cl from "./Gallery.module.css";
 import slide1 from "../../assets/img/flower.jpeg";
 import slide4 from "../../assets/img/flower4.webp";
@@ -10,60 +10,136 @@ import slide3 from "../../assets/img/1.jpg";
 // import slide6 from "../../assets/img/gallery-img3.jpeg";
 import slide6 from "../../assets/img/1.jpg";
 import Title from "../title/Title";
+import { motion } from "framer-motion";
 
 const Gallery = ({ refOf }) => {
+  const componentAnimationRef = useRef({
+    hidden: {
+      x: -100,
+      opacity: 0,
+      transition: { y: { stiffness: 1000 } },
+    },
+    visible: (custom) => ({
+      x: 0,
+      opacity: 1,
+      transition: {
+        delay: custom * 0.2,
+        y: { stiffness: 1000, velocity: -100 },
+      },
+    }),
+  });
+
   return (
-    <>
-      <div ref={refOf} style={{ marginTop: "130px", marginBottom: "100px" }}>
+    <motion.div
+      initial="hidden"
+      whileInView="visible"
+      // variants={componentAnimationRef.current}
+      // viewport={{ once: false }}
+      // custom={1}
+    >
+      <motion.div
+        variants={componentAnimationRef.current}
+        custom={2}
+        ref={refOf}
+        style={{ marginTop: "130px", marginBottom: "100px" }}
+      >
         <Title h2="Wedding Gallery" />
-      </div>
+      </motion.div>
       <section className={cl.slider}>
         <div className={cl.slideTrack}>
-          <div className={cl.slide}>
+          <motion.div
+            variants={componentAnimationRef.current}
+            custom={3}
+            className={cl.slide}
+          >
             <img className={cl.img} src={slide1} alt="img" />
-          </div>
-          <div className={cl.slide}>
+          </motion.div>
+          <motion.div
+            variants={componentAnimationRef.current}
+            custom={4}
+            className={cl.slide}
+          >
             <img className={cl.img} src={slide2} alt="img" />
-          </div>
-          <div className={cl.slide}>
+          </motion.div>
+          <motion.div
+            variants={componentAnimationRef.current}
+            custom={5}
+            className={cl.slide}
+          >
             <img className={cl.img} src={slide3} alt="img" />
-          </div>
-          <div className={cl.slide}>
+          </motion.div>
+          <motion.div
+            variants={componentAnimationRef.current}
+            custom={6}
+            className={cl.slide}
+          >
             <img className={cl.img} src={slide4} alt="img" />
-          </div>
-          <div className={cl.slide}>
+          </motion.div>
+          <motion.div
+            variants={componentAnimationRef.current}
+            custom={7}
+            className={cl.slide}
+          >
             <img className={cl.img} src={slide5} alt="img" />
-          </div>
-          <div className={cl.slide}>
+          </motion.div>
+          <motion.div
+            variants={componentAnimationRef.current}
+            custom={8}
+            className={cl.slide}
+          >
             <img className={cl.img} src={slide6} alt="img" />
-          </div>
+          </motion.div>
           {/* <div className={cl.slide}>
             <img className={cl.img} src={slide7} alt="img" />
           </div> */}
-          <div className={cl.slide}>
+          <motion.div
+            variants={componentAnimationRef.current}
+            custom={9}
+            className={cl.slide}
+          >
             <img className={cl.img} src={slide1} alt="img" />
-          </div>
-          <div className={cl.slide}>
+          </motion.div>
+          <motion.div
+            variants={componentAnimationRef.current}
+            custom={10}
+            className={cl.slide}
+          >
             <img className={cl.img} src={slide2} alt="img" />
-          </div>
-          <div className={cl.slide}>
+          </motion.div>
+          <motion.div
+            variants={componentAnimationRef.current}
+            custom={11}
+            className={cl.slide}
+          >
             <img className={cl.img} src={slide3} alt="img" />
-          </div>
-          <div className={cl.slide}>
+          </motion.div>
+          <motion.div
+            variants={componentAnimationRef.current}
+            custom={12}
+            className={cl.slide}
+          >
             <img className={cl.img} src={slide4} alt="img" />
-          </div>
-          <div className={cl.slide}>
+          </motion.div>
+          <motion.div
+            variants={componentAnimationRef.current}
+            custom={13}
+            className={cl.slide}
+          >
             <img className={cl.img} src={slide5} alt="img" />
-          </div>
-          <div className={cl.slide}>
+          </motion.div>
+          <motion.div
+            variants={componentAnimationRef.current}
+            custom={14}
+            className={cl.slide}
+          >
             <img className={cl.img} src={slide6} alt="img" />
-          </div>
+          </motion.div>
           {/* <div className={cl.slide}>
             <img className={cl.img} src={slide7} alt="img" />
           </div> */}
         </div>
       </section>
-    </>
+    </motion.div>
   );
 };
 
--- src/components/invitation/Invitation.jsx
+++ src/components/invitation/Invitation.jsx
@@ -1,22 +1,53 @@
-import React from "react";
+import React, { useRef } from "react";
 import cl from "./Invitation.module.css";
 import Title from "../title/Title";
 import invitation from "../../assets/icons/invitation.svg";
+import { motion } from "framer-motion";
 
 const Invitation = () => {
+  const componentAnimationRef = useRef({
+    hidden: {
+      x: -100,
+      opacity: 0,
+      transition: { y: { stiffness: 1000 } },
+    },
+    visible: (custom) => ({
+      x: 0,
+      opacity: 1,
+      transition: {
+        delay: custom * 0.2,
+        y: { stiffness: 1000, velocity: -100 },
+      },
+    }),
+  });
+
   return (
-    <section className={cl.container}>
+    <motion.section
+      initial="hidden"
+      whileInView="visible"
+      className={cl.container}
+    >
       <div className={cl.overlay} />
-      <Title style={{ color: "#fff" }} h2="Invitation" />
+      <motion.div variants={componentAnimationRef.current} custom={1}>
+        <Title style={{ color: "#fff" }} h2="Invitation" />
+      </motion.div>
       <div className={cl.innerContainer}>
-        <div className={cl.rightSide}>
+        <motion.div
+          variants={componentAnimationRef.current}
+          custom={2}
+          className={cl.rightSide}
+        >
           <img className={cl.img} src={invitation} alt="invitation" />
           <p>Сохраните </p>
           <span> эту</span>
           <p>Дату! </p>
           <img className={cl.img2} src={invitation} alt="invitation" />
-        </div>
-        <div className={cl.leftSide}>
+        </motion.div>
+        <motion.div
+          variants={componentAnimationRef.current}
+          custom={3}
+          className={cl.leftSide}
+        >
           <h2>
             Kurapika <small>&amp;</small> Alluka
           </h2>
@@ -29,9 +60,9 @@ const Invitation = () => {
             Kaynar, банкетный <br /> зал, Сухэ-Батора, 1/2, Бишкек
           </div>
           <h5>Последующий прием</h5>
-        </div>
+        </motion.div>
       </div>
-    </section>
+    </motion.section>
   );
 };
 
